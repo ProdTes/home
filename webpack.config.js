68751c1345c1433338535d40b040a08b38f1835a
ProdTes <111216191+ProdTes@users.noreply.github.com>
2022-08-14 11:26:15 +0530

    Add files via upload

---
+++ webpack.config.js
@@ -0,0 +1,75 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const { ModuleFederationPlugin } = require("webpack").container;
+
+const deps = require("./package.json").dependencies;
+module.exports = {
+
+  output: {
+    publicPath: "http://localhost:3000/",
+  },  
+  performance: {
+    maxAssetSize: 500000,
+    maxEntrypointSize: 500000,
+    hints: false,
+  },
+
+  resolve: {
+    extensions: [".tsx", ".ts", ".jsx", ".js", ".json"],
+  },
+
+  devServer: {
+    port: 3000,
+    historyApiFallback: true,
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.m?js/,
+        type: "javascript/auto",
+        resolve: {
+          fullySpecified: false,
+        },
+      },
+      {
+        test: /\.(css|s[ac]ss)$/i,
+        use: ["style-loader", "css-loader", "postcss-loader"],
+      },
+      {
+        test: /\.(ts|tsx|js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ['@babel/preset-react'],
+          }
+        }
+      },
+    ],
+  },
+
+  plugins: [
+    new ModuleFederationPlugin({
+      name: "app2",
+        library: { type: "umd", name: "app2" },
+      filename:"remoteEntry.js",
+      exposes: {
+        "./Header": "./src/Header",
+        "./Footer": "./src/Footer",
+      },
+      shared: {
+        "react":{
+          eager: true
+
+      }, "react-dom":{
+        eager: true
+
+      },"tailwindcss":{
+      }},
+   
+    }),
+    new HtmlWebPackPlugin({
+      template: "./src/index.html",
+    }),
+  ],
+};
---
+++ webpack.configold.js
@@ -0,0 +1,62 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+
+const deps = require("./package.json").dependencies;
+module.exports = {
+  output: {
+    publicPath: "http://localhost:3000/",
+  },
+
+  resolve: {
+    extensions: [".tsx", ".ts", ".jsx", ".js", ".json"],
+  },
+
+  devServer: {
+    port: 3000,
+    historyApiFallback: true,
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.m?js/,
+        type: "javascript/auto",
+        resolve: {
+          fullySpecified: false,
+        },
+      },
+      {
+        test: /\.(css|s[ac]ss)$/i,
+        use: ["style-loader", "css-loader", "postcss-loader"],
+      },
+      {
+        test: /\.(ts|tsx|js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-react"],
+          }
+        },
+      },
+    ],
+  },
+
+  plugins: [
+    new ModuleFederationPlugin({
+      name: "home",
+      filename: "remoteEntry.js",
+      remotes: {
+        home: "home@http://localhost:3000/remoteEntry.js",
+      },
+      exposes: {
+        "./Header": "./src/Header",
+        "./Footer": "./src/Footer",
+      },
+      shared: { react: { singleton: true }, 'react-dom': { singleton: true } },
+    }),
+    new HtmlWebPackPlugin({
+      template: "./src/index.html",
+    }),
+  ],
+};
